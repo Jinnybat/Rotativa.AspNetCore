844e5919797a9d4c77bf2ea7c344e27039f5d10d
Deepak <guitarjinny@gmail.com>
2023-12-15 17:12:39 +0530

    Initialize

---
+++ Rotativa.AspNetCore.Demo/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Rotativa.AspNetCore.Demo.Models;
+
+namespace Rotativa.AspNetCore.Demo.Controllers
+{
+    public class TestModel
+    {
+        public string Name { get; set; }
+    }
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            var model = new TestModel { Name = "Giorgio" };
+            return new ViewAsPdf(model, ViewData);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return new ViewAsPdf("~/Views/Home/Contact.cshtml", viewData: ViewData);
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Rotativa.AspNetCore/RotativaConfig.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Rotativa.AspNetCore
+{
+    public static class RotativaConfiguration
+    {
+        private static string _RotativaPath;
+        internal static string RotativaPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_RotativaPath))
+                {
+                    throw new ArgumentNullException(nameof(RotativaPath), "Check the readme file for configuration instructions");
+                }
+                return _RotativaPath;
+            }
+        }
+
+#if NETSTANDARD2_0
+        /// <summary>
+        /// Setup Rotativa library
+        /// </summary>
+        /// <param name="env">The IHostingEnvironment object</param>
+        /// <param name="wkhtmltopdfRelativePath">Optional. Relative path to the directory containing wkhtmltopdf.exe. Default is "Rotativa". Download at https://wkhtmltopdf.org/downloads.html</param>
+        public static void Setup(Microsoft.AspNetCore.Hosting.IHostingEnvironment env, string wkhtmltopdfRelativePath = "Rotativa") 
+        {
+            var rotativaPath = Path.Combine(env.WebRootPath, wkhtmltopdfRelativePath);
+
+            if (!Directory.Exists(rotativaPath))
+            {
+                throw new ApplicationException("Folder containing wkhtmltopdf.exe not found, searched for " + rotativaPath);
+            }
+
+            _RotativaPath = rotativaPath;
+        }
+#endif
+
+        /// <summary>
+        /// Setup Rotativa library
+        /// </summary>
+        /// <param name="rootPath">The path to the web-servable application files.</param>
+        /// <param name="wkhtmltopdfRelativePath">Optional. Relative path to the directory containing wkhtmltopdf.exe. Default is "Rotativa". Download at https://wkhtmltopdf.org/downloads.html</param>
+        public static void Setup(string rootPath, string wkhtmltopdfRelativePath = "Rotativa")
+        {
+            var rotativaPath = Path.Combine(rootPath, wkhtmltopdfRelativePath);
+
+            if (!Directory.Exists(rotativaPath))
+            {
+                throw new ApplicationException("Folder containing wkhtmltopdf.exe not found, searched for " + rotativaPath);
+            }
+
+            _RotativaPath = rotativaPath;
+        }
+
+    }
+}
